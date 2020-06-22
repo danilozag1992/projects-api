282f3fedf79be12d66754da6f018d7c5be5b0dd9
danilozag1992 <danilozagarcanin@gmail.com>
2020-06-22 13:50:50 +0200

    testing access token

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:login) { |n| "dzagarcanin #{n}" }
+    sequence(:login) { |n| "dzagarcanin#{n}" }
     name { "Danilo Zagarcanin" }
     url { "http://example.com" }
     avatar_url { "http://example.com/avatar" }
---
+++ spec/models/access_token_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe AccessToken, type: :model do
+  describe '#validations' do
+    it 'should have valid factory' do
+
+    end
+
+    it 'should validate token' do
+
+    end
+  end
+end
